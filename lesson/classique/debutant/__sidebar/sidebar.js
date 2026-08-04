7d7bcd55239dd60d343f5731042317352ab20339
tboidron <tboidron@gmail.com>
2026-08-04 20:24:45 +0200

    Mise à jour automatique

--- lesson/classique/debutant/__sidebar/sidebar.js
+++ lesson/classique/debutant/__sidebar/sidebar.js
@@ -1,14 +1,38 @@
 fetch("../__sidebar/sidebar.html")
-    .then(response => response.text())
-    .then(html => {
+.then(response => response.text())
+.then(html =>
+{
+    document.getElementById("sidebar").innerHTML = html;
 
-        document.getElementById("sidebar").innerHTML = html;
+    const details = document.querySelectorAll(".lesson-menu details");
 
-        const current = document.body.dataset.lesson;
+    details.forEach(detail =>
+    {
+        detail.querySelector("summary").addEventListener("click", function ()
+        {
+            // Si on va ouvrir ce chapitre
+            if (!detail.open)
+            {
+                details.forEach(other =>
+                {
+                    if (other !== detail)
+                        other.removeAttribute("open");
+                });
+            }
+        });
+    });
 
-        document
-            .querySelector(`a[data-lesson="${current}"]`)
-            ?.classList.add("current");
+    const current = document.body.dataset.lesson;
 
-    })
-    .catch(console.error);
+    const currentLink = document.querySelector(
+        `a[data-lesson="${current}"]`
+    );
+
+    if (currentLink)
+    {
+        currentLink.classList.add("current");
+
+        currentLink.closest("details").setAttribute("open", "");
+    }
+})
+.catch(console.error);
